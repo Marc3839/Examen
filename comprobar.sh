e9b66e0480b9295108405839c45db7c1a92030ef
Wei <weiluo@132.com>
2020-05-04 19:45:27 +0200

    Otro direfente pero tampoco funciona

--- comprobar.sh
+++ comprobar.sh
@@ -4,17 +4,17 @@ pid=$2
 c=1
 maximo=$(cat proceso.txt | wc -l )
 while [ $c -le $maximo ];do
-    
-    
-    linea1=$(cat proceso.txt | head -$contador | tail -1) 
-    lineaU=$(echo $linea1 | awk '{print $1}')
-    lineaP=$(echo $linea1 | awk '{print $2}')
-
-    if [ $us= $lineaU ]; then
-	if [ $pid= $lineaP ];then
-	    echo "$us es correspondiente con PID $pid"
+	linea1=$(cat proceso.txt | head -$c | tail -1) 
+	lineaP=$(echo $linea1 | awk '{print $2}')
+	lineaU=$(echo $linea1 | awk '{print $1}')
+	
+        if [ $us= $lineaU ]; then
+	    if [ $pid= $lineaP ];then
+		echo "$us es correspondiente con PID $pid"
+		exit
 	else
 	    echo "$us no es correspondiente con PID $pid"
 	fi
-    fi
+	fi
+    done
 done
